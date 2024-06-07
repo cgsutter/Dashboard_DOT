0e7e47dee28f3079c5bc94b666d84204f2ec43f6
csutter <csutter@albany.edu>
2024-06-07 22:39:09 +0000

    Added toggle to use the dictionary of conditions (color coded by location), set up with 3 different data files of conditions, current, 60 min in past, and 60 min in future a forecast. Did not tie in actual model prediction colors yet, just dummy files of dictionary of site ID and color

--- src/Map.js
+++ src/Map.js
@@ -3,7 +3,9 @@ import React, { useState, useRef, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import camdata from "../data/dot_cam_latlon.js";
-import camdatacolor from "../data/dot_cam_latlon_color_current.js";
+import camdatacolor_current from "../data/dot_cam_latlon_color_current.js";
+import camdatacolor_prior60 from "../data/dot_cam_latlon_color_prior60min.js";
+import camdatacolor_future60 from "../data/dot_cam_latlon_color_future60min.js";
 import { api_token_mapbox } from '../credentials.js';
 // const token = MY_CONSTANT
 mapboxgl.accessToken = api_token_mapbox;
@@ -13,10 +15,12 @@ mapboxgl.accessToken = api_token_mapbox;
 const Map = (props) => {
     const mapContainer = useRef(null);
     const { state } = props;
-    // const [toggle, setToggle] = useState(true); // Add a state variable for the toggle
+    const [toggle, setToggle] = useState(true); // Add a state variable for the toggle
+    const [selectedDictionary, setSelectedDictionary] = useState('camdatacolor_current'); 
+
 
     console.log("PRINT COLOR")
-    console.log(camdatacolor['Skyline_5996'])
+    console.log(camdatacolor_current['Skyline_5996'])
     
 
     useEffect(() => {
@@ -56,7 +60,9 @@ const Map = (props) => {
                 const el = document.createElement('div');
                 el.className = 'marker';
                 // el.style.background = 'red';
-                el.style.background = camdatacolor[(entry.id)];
+                // el.style.background = camdatacolor_current[(entry.id)];
+                el.style.background = selectedDictionary === 'camdatacolor_current' ? camdatacolor_current[entry.id] : selectedDictionary === 'camdatacolor_prior60' ? camdatacolor_prior60[entry.id] : camdatacolor_future60[entry.id];
+
                 el.style.width = '10px';
                 el.style.height = '10px';
                 el.style.borderRadius = '50%';
@@ -71,7 +77,7 @@ const Map = (props) => {
         }
 
         return () => map.remove();
-    }, [state]);
+    }, [state, toggle, selectedDictionary]);
 
     useEffect(() => {
         if (mapContainer.current && props.state.lng && props.state.lat) {
@@ -79,19 +85,23 @@ const Map = (props) => {
         }
     }, [mapContainer, props.state.lng, props.state.lat]);
 
-    return (
-        <div
-          ref={mapContainer}
-          style={{
-            width: '100%',
-            height: '100%',
-            position: 'relative',
-          }}
-        >
-          {/* Map will render here */}
-        </div>
-      );
-    };
+    const handleDictionaryChange = (event) => {
+        setSelectedDictionary(event.target.value);
+      };
+
+    // return (
+    //     <div
+    //       ref={mapContainer}
+    //       style={{
+    //         width: '100%',
+    //         height: '100%',
+    //         position: 'relative',
+    //       }}
+    //     >
+    //       {/* Map will render here */}
+    //     </div>
+    //   );
+    // };
     // return (
     //     <div>
     //       <button onClick={() => setToggle(!toggle)}>Toggle Dictionary</button> {/* Add a toggle button */}
@@ -108,5 +118,25 @@ const Map = (props) => {
     //     </div>
     //   );
     // };
+    return (
+        <div>
+          <select onChange={handleDictionaryChange} value={selectedDictionary}>
+            <option value="camdatacolor_current">Cam Data Color</option>
+            <option value="camdatacolor_prior60">Past: 60 min ago</option>
+            <option value="camdatacolor_future60">Future: 60 min forecast</option>
+          </select>
+          <div
+            ref={mapContainer}
+            style={{
+              width: '100%',
+              height: '100%',
+              position: 'relative',
+            }}
+          >
+            {/* Map will render here */}
+          </div>
+        </div>
+      );
+    };
 
 export default Map;
