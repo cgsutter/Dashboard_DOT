076f8fc2756691ae8ebc3a5ba7b040f33a775d91
csutter <csutter@albany.edu>
2024-08-20 22:10:50 +0000

    Updated map to include the confidence and condition, hovered when clicked on a site, and also to always plot all DOT cams, but just fill in with black if the model predicted condition for that cam wasnt avail

--- src/Map.js
+++ src/Map.js
@@ -3,9 +3,14 @@ import React, { useState, useRef, useEffect } from 'react';
 import mapboxgl from '!mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import camdata from "../data/dot_cam_latlon.js";
-import camdatacolor_current from "../data/dot_cam_latlon_color_current.js";
-import camdatacolor_prior60 from "../data/dot_cam_latlon_color_prior60min.js";
-import camdatacolor_future60 from "../data/dot_cam_latlon_color_future60min.js";
+import camdata_current from "../data/dot_cam_current.js";
+import camdata_casestudy from "../data/dot_cam_casestudy.js";
+// import camdata_prior60 from "../data/dot_cam_latlon_color_prior60min.js";
+// import camdata_future60 from "../data/dot_cam_latlon_color_future60min.js";
+// import camdata_casestudy1a from "../data/dot_cam_latlon_color_casestudy_1a.js";
+// import camdata_casestudy1b from "../data/dot_cam_latlon_color_casestudy_1b.js";
+// import camdata_casestudy2a from "../data/dot_cam_latlon_color_casestudy_2a.js";
+// import camdata_casestudy2b from "../data/dot_cam_latlon_color_casestudy_2b.js";
 import ColorKey from './ColorKey.js';
 import { api_token_mapbox } from '../credentials.js';
 // const token = MY_CONSTANT
@@ -17,14 +22,16 @@ const Map = (props) => {
   const mapContainer = useRef(null);
   const { state } = props;
   const [toggle, setToggle] = useState(true); // Add a state variable for the toggle
-  const [selectedDictionary, setSelectedDictionary] = useState('camdatacolor_current');
+  const [selectedDictionary, setSelectedDictionary] = useState('camdata_current');
   // const [colorKeyVisible, setColorKeyVisible] = useState(true);
 
 
 
   console.log("PRINT COLOR")
-  console.log(camdatacolor_current['Skyline_5996'])
+  // console.log(camdata_current['Skyline_5996'].color)
 
+  // console.log("try logging here")
+  // console.log(camdata_current[entry.id])
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -62,18 +69,31 @@ const Map = (props) => {
       camdata.forEach(entry => {
         const el = document.createElement('div');
         el.className = 'marker';
-        // el.style.background = 'red';
-        // el.style.background = camdatacolor_current[(entry.id)];
-        el.style.background = selectedDictionary === 'camdatacolor_current' ? camdatacolor_current[entry.id] : selectedDictionary === 'camdatacolor_prior60' ? camdatacolor_prior60[entry.id] : camdatacolor_future60[entry.id];
+
+        const rscData = (
+          selectedDictionary === 'camdata_current'
+            ? camdata_current[entry.id]
+            : selectedDictionary === 'camdata_casestudy'
+            ? camdata_casestudy[entry.id]
+            : null 
+            // null in case of neither being selected
+        ) ?? { final_model_pred: 'NA', color: 'black', confidence: 'NA' };
+
+        el.style.background = rscData.color;
 
         el.style.width = '10px';
         el.style.height = '10px';
         el.style.borderRadius = '50%';
 
+        // console.log("try logging here")
+        // console.log(camdata_current['Skyline_6510'].color)
+
         new mapboxgl.Marker(el)
           .setLngLat([entry.lon, entry.lat])
-          .setPopup(new mapboxgl.Popup().setText(entry.id))
-          .addTo(map);
+          .setPopup(new mapboxgl.Popup().setHTML(`ID: ${entry.id} <br> Condition: ${rscData.final_model_pred} <br>  Confidence: ${rscData.confidence}`))
+          .addTo(map); 
+          // ${camdata_current[entry.id][color]}
+          // ${entry.id}${entry.confidence}
       });
     } else {
       console.error('Data is not an array');
@@ -81,98 +101,99 @@ const Map = (props) => {
 
 
 
-// // Add color key overlay
-// const colorKeyOverlay = new mapboxgl.Overlay({
-//   element: document.getElementById('color-key'),
-//   visible: colorKeyVisible,
-// });
-// map.addOverlay(colorKeyOverlay);
+    // // Add color key overlay
+    // const colorKeyOverlay = new mapboxgl.Overlay({
+    //   element: document.getElementById('color-key'),
+    //   visible: colorKeyVisible,
+    // });
+    // map.addOverlay(colorKeyOverlay);
 
-return () => map.remove();
+    return () => map.remove();
   }, [state, toggle, selectedDictionary]);
 
-useEffect(() => {
-  if (mapContainer.current && props.state.lng && props.state.lat) {
-    // mapContainer.current.style.height = '750px'; 
-  }
-}, [mapContainer, props.state.lng, props.state.lat]);
+  useEffect(() => {
+    if (mapContainer.current && props.state.lng && props.state.lat) {
+      // mapContainer.current.style.height = '750px'; 
+    }
+  }, [mapContainer, props.state.lng, props.state.lat]);
 
-const handleDictionaryChange = (event) => {
-  setSelectedDictionary(event.target.value);
-};
+  const handleDictionaryChange = (event) => {
+    setSelectedDictionary(event.target.value);
+  };
 
-return (
+  return (
 
-  <div>
-    <div id="color-key">
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'red',
-            padding: '5px',
-            borderRadius: '5px'
-          }}>Severe Snow</span>
-        </li>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'palevioletred',
-            padding: '5px',
-            borderRadius: '5px'
-          }}>Snow</span>
-        </li>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'dodgerblue',
-            padding: '5px',
-            borderRadius: '5px'
-          }}>Wet</span>
-        </li>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'green',
-            padding: '5px',
-            borderRadius: '5px'
-          }}>Dry</span>
-        </li>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'purple',
-            padding: '5px',
-            borderRadius: '5px',
-            color: 'white'
-          }}>Poor Visibility</span>
-        </li>
-        <li style={{ marginBottom: '12px' }}>
-          <span style={{
-            backgroundColor: 'darkblue',
-            padding: '5px',
-            borderRadius: '5px',
-            color: 'white'
-          }}>Obstructed</span>
-        </li>
-      </ul>
-    </div>
-    <select onChange={handleDictionaryChange} value={selectedDictionary}>
-      <option value="camdatacolor_current">Current (using camera)</option>
-      <option value="camdatacolor_prior60">Past: 60 min ago (using camera)</option>
-      <option value="camdatacolor_future60">Future: 60 min forecast (using weather forecast)</option>
-    </select>
-    <div
-      ref={mapContainer}
-      style={{
-        width: '80vw',
-        height: '80vh',
-        position: 'relative',
-      }}
-    >
-      {/* <div id="color-key">
+    <div>
+      <div id="color-key">
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'red',
+              padding: '5px',
+              borderRadius: '5px'
+            }}>Severe Snow</span>
+          </li>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'palevioletred',
+              padding: '5px',
+              borderRadius: '5px'
+            }}>Snow</span>
+          </li>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'dodgerblue',
+              padding: '5px',
+              borderRadius: '5px'
+            }}>Wet</span>
+          </li>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'green',
+              padding: '5px',
+              borderRadius: '5px'
+            }}>Dry</span>
+          </li>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'purple',
+              padding: '5px',
+              borderRadius: '5px',
+              color: 'white'
+            }}>Poor Visibility</span>
+          </li>
+          <li style={{ marginBottom: '12px' }}>
+            <span style={{
+              backgroundColor: 'darkblue',
+              padding: '5px',
+              borderRadius: '5px',
+              color: 'white'
+            }}>Obstructed</span>
+          </li>
+        </ul>
+      </div>
+      <select onChange={handleDictionaryChange} value={selectedDictionary}>
+        <option value="camdata_current">Current (using camera)</option>
+        <option value="camdata_casestudy">Case study example</option>
+        {/* <option value="camdata_future60">Future: 60 min forecast (using weather forecast)</option> */} 
+        {/* Past: 60 min ago (using camera) */}
+      </select>
+      <div
+        ref={mapContainer}
+        style={{
+          width: '80vw',
+          height: '80vh',
+          position: 'relative',
+        }}
+      >
+        {/* <div id="color-key">
         <ColorKey />
       </div> */}
-      {/* Map will render here */}
-    </div>
+        {/* Map will render here */}
+      </div>
 
-  </div>
-);
+    </div>
+  );
 
 };
 
