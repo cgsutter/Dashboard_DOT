ab110e500f4f88b4d61b1beb9f96112ca326e6af
csutter <csutter@albany.edu>
2024-11-04 21:51:09 +0000

    Pushing before reorganizing to fix the map render issue

--- src/Map.js
+++ src/Map.js
@@ -15,10 +15,21 @@ const Map = (props) => {
   const mapContainer = useRef(null);
   const { state } = props;
   const [toggle, setToggle] = useState(true); // Add a state variable for the toggle
-  const [selectedDictionary, setSelectedDictionary] = useState('dot_cam_current');
+  const [selectedDictionary, setSelectedDictionary] = useState('dot_cam_current'); // setSelectedDictionary is the function that that runs to change selectedDictionary
   const [data, setData] = useState({}); // State to store API data
 
-
+  const getTitle = () => {
+    switch (selectedDictionary) {
+      case 'dot_cam_current':
+        return 'Current road surface conditions';
+      case 'camdata_casestudy_20220203_06':
+        return 'Case Study: Feb 3 2022 at 1am EST';
+      case 'camdata_casestudy_20220203_18':
+        return 'Case Study: Feb 3 2022 at 1pm EST';
+      default:
+        return 'Road surface conditions';
+    }
+  };
 
 
   // const fetchData = async () => {
@@ -60,9 +71,16 @@ const Map = (props) => {
         headers: {
           'Content-Type': 'application/json'
         }
-      }).then(async (res)=> await console.log('res',res.json()));
+      })
+      // .then(async (res)=> await console.log('res',res.json()));
       console.log("got through await fetch")
+      console.log(response.status)
+     
+      const data_readfromapi = await response.json();
+      console.log("got through await response")
+
       //comment out response parts to get UI to load
+      console.log('Full Response:', response);
       console.log('Response Type:', typeof response); // "object"
       console.log('Response Type22:', typeof response); // "object"
       console.log('Response Constructor:', response.constructor.name); // "Response"
@@ -72,10 +90,6 @@ const Map = (props) => {
       console.log('Response Body Used:', response.bodyUsed);
       console.log(response.ok)
       console.log(response.status)
-      const data_readfromapi = await response.json();
-      console.log("got through await response")
-
-
 
       // console.log('Data Type:', typeof data_readfromapi); // "object"
       // console.log('JSON Data:', data_readfromapi);
@@ -98,25 +112,16 @@ const Map = (props) => {
     fetchData();
   }, [selectedDictionary]);
 
+  console.log("print data")
+  console.log(data)
+
   // const handleDictionaryChange = (event) => {
   //   setSelectedDictionary(event.target.value);
   // };
 
-  console.log('print dictionary here')
-  console.log(selectedDictionary)
+  // console.log('print dictionary here')
+  // console.log(selectedDictionary)
 
-  const getTitle = () => {
-    switch (selectedDictionary) {
-      case 'dot_cam_current':
-        return 'Current road surface conditions';
-      case 'camdata_casestudy_20220203_06':
-        return 'Case Study: Feb 3 2022 at 1am EST';
-      case 'camdata_casestudy_20220203_18':
-        return 'Case Study: Feb 3 2022 at 1pm EST';
-      default:
-        return 'Road surface conditions';
-    }
-  };
 
 
   
@@ -125,8 +130,8 @@ const Map = (props) => {
     snow: true,
     wet: true,
     dry: true,
-    poor_viz: false,
-    obs: false,
+    poor_viz: true,
+    obs: true,
   });
 
   const handleConditionChange = (event) => {
@@ -135,6 +140,9 @@ const Map = (props) => {
       [event.target.name]: event.target.checked,
     }));
   };
+
+  console.log("log conditions")
+  console.log(conditions)
 
   useEffect(() => {
     const map = new mapboxgl.Map({
@@ -179,12 +187,15 @@ const Map = (props) => {
         "snow_severe",
       ];
 
+      // plot each dot of conditions on layered order (most severe up top)
+      // e.g. all the dry conditions, grab cam entries who have dry condition, and plot them. Do this for each condition (the .forEach is essentially a loop)
       conditionOrder.forEach((condition) => {
         camdata.forEach((entry) => {
+          // for each lat lon in camdata (which is just site, lat, lon), grab the corresponding conditions from the clasification dictionary (setData) by grabbing the key entry.id and its corresponding values 
           const rscData = (
             data[entry.id] ?? { final_model_pred: "NA", color: "black", confidence: "NA" }
           );
-
+          // check if the classification is the condition that we're currently looping through (can probably rearrange this code eventually since this could be outside the main loop)
           if (rscData.final_model_pred === condition && conditions[condition.toLowerCase()]) {
             const el = document.createElement("div");
             el.className = "marker";
@@ -208,7 +219,15 @@ const Map = (props) => {
 
     return () => map.remove();
   }, [state, toggle, selectedDictionary, conditions]);
-
+// [state, toggle, selectedDictionary, conditions]
+  console.log("log state")
+  console.log(state)
+  console.log("log toggle")
+  console.log(toggle)
+  console.log("log selectedDictionar")
+  console.log(selectedDictionary)
+  console.log("log conditions")
+  console.log(conditions)
   useEffect(() => {
     if (mapContainer.current && props.state.lng && props.state.lat) {
       // mapContainer.current.style.height = '750px'; 
