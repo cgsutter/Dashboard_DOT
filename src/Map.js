3cb47d38cc529591ac66367f54bc91e0bf110eb3
csutter <csutter@albany.edu>
2024-11-06 20:41:37 +0000

    First working version of the added forecast underlay, read in through API. Did not add forecast ONLY toggle yet

--- src/Map.js
+++ src/Map.js
@@ -8,14 +8,16 @@ import { api_token_mapbox } from '../credentials.js';
 // const token = MY_CONSTANT 
 mapboxgl.accessToken = api_token_mapbox;
 
-import axios from 'axios';
 
 // This entire Map function is called on in App.js. Inputs are props (state) which is macro map details, like centered lat/lon and zoom. 
 const Map = (props) => {
   const mapContainer = useRef(null);
   const { state } = props; 
+  // const [autoUpdate, setAutoUpdate] = useState(false); // Track if auto-update is enabled
   const [selectedDictionary, setSelectedDictionary] = useState('dot_cam_current'); // selectedDictionary is the for the corresponding case that the user selected
+  const [selectedFCSTDictionary, setSelectedFCSTDictionary] = useState('FCST_current'); // equivalent of the above but for fcst data
   const [data, setData] = useState({}); // Based on the selectedDictionary, load the corresponding data using the API and store it in data
+  const [FCSTdata, setFCSTData] = useState({}); // equivalent of the above but for fcst data
 
   // Return text for the dashboard title based on the user's selected case
   const getTitle = () => {
@@ -56,8 +58,29 @@ const Map = (props) => {
 
   // Define this function to handle whichever case the user wants to map, based on their selection from the dropdown dashbaord
   const handleDictionaryChange = (event) => {
-    setSelectedDictionary(event.target.value);
+    const value = event.target.value;
+    setSelectedDictionary(value);
+
+    //  TO COME BACK TO: I think the map is loading twice bc of how the fcst dict changes AFTER the regular data dict changes
+    //  add the second piece taht we want the toggle to adjust, the forecast only json file name too (not just the cam level one)
+    if (value === "dot_cam_current") {
+      setSelectedFCSTDictionary("FCST_current");
+    } else if (value === "camdata_casestudy_20220203_06") {
+      setSelectedFCSTDictionary("FCST_casestudy_20220203_06");
+    } else if (value === "camdata_casestudy_20220203_18") {
+      setSelectedFCSTDictionary("FCST_casestudy_20220203_18");
+    }
   };
+
+  console.log("CHECK THE FCST ONLY DICT NAME!")
+  console.log(selectedFCSTDictionary)
+
+  // // Function to toggle auto-update on/off based on checkbox input
+  // const handleAutoUpdateChange = (event) => {
+  //   setAutoUpdate(event.target.checked);
+  // };
+
+  
 
   // Second: Load in the data
 
@@ -75,22 +98,23 @@ const Map = (props) => {
       })
       // .then(async (res)=> await console.log('res',res.json()));
       console.log("got through await fetch")
-      console.log(response.status)
+      // console.log(response.status)
      
       const data_readfromapi = await response.json();
       console.log("got through await response")
 
       //comment out response parts to get UI to load
-      console.log('Full Response:', response);
-      console.log('Response Type:', typeof response); // "object"
-      console.log('Response Type22:', typeof response); // "object"
-      console.log('Response Constructor:', response.constructor.name); // "Response"
-      console.log(response["NYSDOT_4861013"])
-      console.log('Response Status Text:', response.statusText);
-      console.log('Response Headers:', response.headers);
-      console.log('Response Body Used:', response.bodyUsed);
-      console.log(response.ok)
-      console.log(response.status)
+      // // print stuff to check it
+      // console.log('Full Response:', response);
+      // console.log('Response Type:', typeof response); // "object"
+      // console.log('Response Type22:', typeof response); // "object"
+      // console.log('Response Constructor:', response.constructor.name); // "Response"
+      // console.log(response["NYSDOT_4861013"])
+      // console.log('Response Status Text:', response.statusText);
+      // console.log('Response Headers:', response.headers);
+      // console.log('Response Body Used:', response.bodyUsed);
+      // console.log(response.ok)
+      // console.log(response.status)
 
       // console.log('Data Type:', typeof data_readfromapi); // "object"
       // console.log('JSON Data:', data_readfromapi);
@@ -101,6 +125,34 @@ const Map = (props) => {
       // console.log('API Response:', dataReadFromAPI);
       // const jsonData = JSON.parse(dataReadFromAPI); // Attempt parsing
       // setData(jsonData);
+      // console.log('after setdta should exist:', data);
+    } catch (error) {
+      console.error('API Error:', error.message);
+    }
+  };
+
+  const fetchFCST = async () => {
+    try {
+      console.log("beginning FCST fetch")
+      console.log(selectedFCSTDictionary)
+      const response = await fetch(`https://xcitemain.asrc.albany.edu/rnode/dgx-a100/3009/data?param=${selectedFCSTDictionary}`, {
+        method: 'GET',
+        // credentials: 'include', // Include cookies
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      // .then(async (res)=> await console.log('res',res.json()));
+      console.log("got through await fetch FCST")
+      // console.log(response.status)
+     
+      const data_readfromapi = await response.json();
+      console.log("got through await response FCST")
+
+      // console.log('try printing in map when pulling data from api');
+      console.log('FCST JSON Data:', data_readfromapi);
+      setFCSTData(data_readfromapi);
+      console.log('after setFCST data FCSTdata should exist:', FCSTdata);
     } catch (error) {
       console.error('API Error:', error.message);
     }
@@ -108,12 +160,26 @@ const Map = (props) => {
 
   // Load the data
   // Do this by calling the fetchdata function when selectedDictionary changes (based on user intraction), and do this by using the built in React useEffect feature
+
+ 
+
   useEffect(() => {
+    // load camspot data
     console.log('Component rendered, fetch should occur');
     console.log('selectedDictionary:', selectedDictionary);
     fetchData();
   }, [selectedDictionary]);
     
+
+  useEffect(() => {
+    // load fcst data. Could put this in the same useeffect above since if one dict changes the other one does too
+    console.log('Component rendered for FCST, fetch should occur for FCST');
+    console.log('selectedFCSTDictionary:', selectedFCSTDictionary);
+    fetchFCST();
+    console.log("IMP CHECK HERE")
+    console.log(FCSTdata)
+  }, [selectedDictionary,selectedFCSTDictionary]);
+
 
   // Third: plot the map. Make two helper functions to break up the code from useEffect (1, the map basics and 2, the plotted RSC points)
 
@@ -181,14 +247,92 @@ const Map = (props) => {
     });
   };
 
+  // Define helper functions to plot fcst color gradients
+  // first helper function will convert the dataFCST dictionary to GeoJSON format for easy Map plotting
+  const convertDataToGeoJSON = (datatoconvert) => {
+    return {
+      type: 'FeatureCollection',
+      features: Object.entries(datatoconvert).map(([key, properties]) => {
+        const [lat, lon] = key.split('_').map(Number);
+        return {
+          type: 'Feature',
+          geometry: {
+            type: 'Point',
+            coordinates: [lon, lat] // Mapbox expects [lon, lat]
+          },
+          properties: {
+            color: properties.color || "#000000" // Default color if missing
+          }
+        };
+      })
+    };
+  };
+
+  // Convert data to GeoJSON format if the FCST data changes (which will happen after the fcst dict changes, which will happen after the user sleects dropdown)
+  // useEffect(() => {
+  //   geoJSONData = convertDataToGeoJSON(FCSTdata);
+  // }, [FCSTdata]);
+
+  const setupPlotFCSTGradient = (map, geodata) => {
+    // Add points data as a source
+    map.addSource('points', {
+      type: 'geojson',
+      data: geodata
+    });
+
+    // Add circle layer for points with gradient effect
+    map.addLayer({
+      id: 'point-layer',
+      type: 'circle',
+      source: 'points',
+      paint: {
+        'circle-color': ['get', 'color'],
+        'circle-radius': 10, // adjust radius as needed for gradient spread, of 15 or 20 will make the points larger, causing more overlap and blending between adjacent points.
+        'circle-opacity': 0.1, // make circles translucent, higher opacity (high values) vs lower opacity (more translucent, higher values here)
+        'circle-blur': 1 // create a gradient-like blur effect A higher blur value gives a more “fuzzy” appearance, creating a gradient effect around each point. Lower blur values will make the edges sharper and more distinct.
+
+      }
+    });
+  };
+    
+
   useEffect(() => {
     const map = setupMap(mapContainer.current, state);
-    setupPlotPoints(map, camdata, data, conditions, [
-      "obs", "poor_viz", "dry", "wet", "snow", "snow_severe"
-    ]);
+    
+    map.on('load', () => {
+      // Convert FCSTdata to GeoJSON format
+
+      console.log("check before geojson conversion")
+      console.log(FCSTdata)
+      const geoJSONData = convertDataToGeoJSON(FCSTdata);
+  
+      console.log ("loaded geojson data")
+      console.log("GeoJSON Data:", geoJSONData);
+      // Plot the forecast gradient using the converted geoJSONData after map load
+      setupPlotFCSTGradient(map, geoJSONData);
+  
+      // Plot points for camdata
+      setupPlotPoints(map, camdata, data, conditions, [
+        "obs", "poor_viz", "dry", "wet", "snow", "snow_severe"
+      ]);
+    });
+
+    // const geoJSONData = convertDataToGeoJSON(FCSTdata);
+    // setupPlotFCSTGradient(map, geoJSONData);
+    // setupPlotPoints(map, camdata, data, conditions, [
+    //   "obs", "poor_viz", "dry", "wet", "snow", "snow_severe"
+    // ]);
   
     return () => map.remove();
-  }, [state, data, conditions]); // update if the state changes (i.e. if map should be centered differently), but most commonly, update if the user defined selections change, either 1) the selected dropdown case, which affects the dictionary name, which affects the data to read in, and 2) which RSC classes to change
+  }, [state, data, FCSTdata, conditions]); // update if the state changes (i.e. if map should be centered differently), but most commonly, update if the user defined selections change, either 1) the selected dropdown case, which affects the dictionary name, which affects the data to read in, and 2) which RSC classes to change
+
+  // useEffect(() => {
+  //   const map = setupMap(mapContainer.current, state);
+  //   // setupPlotFCSTGradient(map, FCSTdata);
+  //   setupPlotFCSTGradient(map, FCSTdata);
+  
+  //   return () => map.remove();
+  // }, [state, FCSTdata, conditions]);
 
   console.log("log selectedDictionar")
   console.log(selectedDictionary)
